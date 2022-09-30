f5b737cf0abed6fbd76ae0e72407d69419d554b0
aul14 <rahmanaulia336@gmail.com>
2022-09-30 10:44:48 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,103 @@
+const { Client, LocalAuth } = require("whatsapp-web.js");
+const fs = require("fs");
+const express = require("express");
+const { body, validationResult } = require("express-validator");
+const qrcode = require("qrcode");
+const socketIO = require("socket.io");
+const http = require("http");
+const fs = require("fs");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const SESSION_FILE_PATH = "./whatsapp-session.json";
+
+let sessionCfg;
+if (fs.existsSync(SESSION_FILE_PATH)) {
+  sessionCfg = require(SESSION_FILE_PATH);
+}
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname });
+});
+const client = new Client({
+  restartOnAuthFail: true,
+  puppeteer: {
+    headless: true,
+  },
+  authStrategy: new LocalAuth(),
+});
+
+client.on("message", (msg) => {
+  if (msg.body == "!ping") {
+    msg.reply("pong");
+  } else if (msg.body == "!kuy") {
+    msg.reply("ayo ke hotel");
+  }
+});
+
+client.initialize();
+
+// SOCKET IO
+io.on("connection", function (socket) {
+  socket.emit("message", "Connecting...");
+
+  client.on("qr", (qr) => {
+    // Generate and scan this code with your phone
+    console.log("QR RECEIVED", qr);
+    qrcode.toDataURL(qr, (err, url) => {
+      socket.emit("qr", url);
+      socket.emit("message", "QR code received, scan please!");
+    });
+  });
+
+  client.on("ready", () => {
+    socket.emit("ready", "Whatsapp is ready!");
+    socket.emit("message", "Whatsapp is ready!");
+  });
+
+  client.on("authenticated", () => {
+    socket.emit("authenticated", "Whatsapp is authenticated!");
+    socket.emit("message", "Whatsapp is authenticated!");
+    console.log("AUTHENTICATED");
+  });
+
+  client.on("auth_failure", function (session) {
+    socket.emit("message", "Auth failure, restarting...");
+  });
+
+  client.on("disconnected", (reason) => {
+    socket.emit("message", "Whatsapp is disconnected!");
+    client.destroy();
+    client.initialize();
+  });
+});
+
+// SEND MESSAGE
+app.post("/send-message", (req, res) => {
+  const number = req.body.number;
+  const message = req.body.message;
+
+  client
+    .sendMessage(number, message)
+    .then((response) => {
+      res.status(200).json({
+        status: true,
+        response: response,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        status: false,
+        response: err,
+      });
+    });
+});
+
+server.listen(8000, function () {
+  console.log("App running on *: " + 8000);
+});
